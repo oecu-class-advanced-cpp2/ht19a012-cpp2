c6b8333d9a1e0c38512c2f9b12e37fc943b3cea6
ota masahide <ht19a012@oecu.jp>
2021-11-11 17:51:10 +0900

    課題

--- src/kadai.cpp
+++ src/kadai.cpp
@@ -3,39 +3,43 @@
 
 #define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。 
 
-bool Prime(int num)
+bool IsPrime(int num)
 {
-	if (num < 2) return false;
-	else if (num == 2) return true;
-	else if (num % 2 == 0) return false; 
-	double sqrtNum = sqrt(num);
-	for (int i = 3; i <= sqrtNum; i += 2)
-	{
-		if (num % i == 0)
-		{
-			return false;
-		}
-	}
-	return true;
+  if (num < 2) return false;
+  else if (num == 2) return true;
+  else if (num % 2 == 0) return false;
+  double sqrtNum = sqrt(num);
+  for (int i = 3; i <= sqrtNum; i += 2) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
-/* --------------------------------------------------------------- */
-/*
- *  nth_prime
- *
- *  与えられた正整数 a と d と n に対して、この等差数列に含まれる n 番目の
- *  素数を返す。
- *
- *  TODO: この nth_prime 関数を実装せよ。必要であれば他に関数や
- *  ファイルを作成せよ。
- */
- /* -------------------------------------------------------------- */
-int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
-    
-};
+int nth_prime(unsigned int a, unsigned int d, unsigned int n)
+{
+  int g, count = 0;
+  for (int i = 0; i < CPP2_PRIME_UPPER_LIMIT; i++) {
+    g = a + (i * d);
+    if (IsPrime(g)) {
+      count++;
+    }
+    if (count == n) {
+      break;
+    }
+  }
+  return g;
+}
 
-
-int main() {
-	std::cout << nth_prime(367,186,151) << std::endl;
-
-	return 0;
+int main()
+{
+  std::cout << nth_prime(367, 186, 151) << std::endl;
+  std::cout << nth_prime(179, 10, 203) << std::endl;
+  std::cout << nth_prime(271, 37, 39) << std::endl;
+  std::cout << nth_prime(103, 230, 1) << std::endl;
+  std::cout << nth_prime(27, 104, 185) << std::endl;
+  std::cout << nth_prime(253, 50, 85) << std::endl;
+// 以下、同様に、入出力例通りになるか確認せよ。 
+  std::cin.get();
+  return 0;
 }
